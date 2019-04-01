2a352522835947a369a5deb3194bacd09f47d6e3
raviolin <raviolin@icloud.com>
2019-04-01 17:33:46 +0900

    0.2.0

--- config/index.js
+++ config/index.js
@@ -1,5 +1,32 @@
 const nconf = require('nconf');
 const path = require('path');
-nconf.argv().env().file({ file: path.join(__dirname, 'config.json') });
-nconf.set('config:mongo:options:reconnectTries', Infinity);
+nconf.argv().env().file({ file: path.join(__dirname, 'config.json') }).defaults({
+    config: {
+        sources: ['MIS'],
+        timeout: 900000,
+        rmis: {
+            allowedServices: [
+                'appointment-ws/appointment',
+                'employees-ws/service',
+                'departments-ws/departments',
+                'individuals-ws/individuals',
+                'locations-ws/resources',
+                'services-ws/services',
+                'patients-ws/patient',
+                'refbooks-ws/refbooksWS',
+                'rooms-ws/rooms'
+            ]
+        },
+        mongo: {
+            options: {
+                reconnectTries: Infinity,
+                socketTimeoutMS: 10000,
+                keepAlive: true,
+                autoIndex: false,
+                useNewUrlParser: true
+            }
+        },
+        ignoreLog: ['Поле СНИЛС не заполнено']
+    }
+});
 module.exports = nconf;
